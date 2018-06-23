ebf87f6c540a3a28a38fd49bc3d9d997cdaa3368
a-nakajima <a-nakajima@shokuryu.co.jp>
2018-06-23 14:44:27 +0900

    .

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './lib/App';
-import connectedApp from './lib/connectedApp';
+import connectedApp from './lib/redux-app';
 Promise.resolve().then(e =>
 {
 	const app = connectedApp(App);
